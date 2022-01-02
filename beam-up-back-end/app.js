e60da478fd631e94f82dbf108c5e168575baf00e
Ram Montilia Smithson <ramsmithson1@gmail.com>
2022-01-02 11:38:33 +0200

    re adding port to app and the heroku

--- beam-up-back-end/app.js
+++ beam-up-back-end/app.js
@@ -6,7 +6,7 @@ const mongoDB = require("./src/routes/mongoDB")
 require('dotenv').config()
 
 const app = express()
-// const port = process.env.Port || 5000
+const port = process.env.Port
 
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
@@ -24,5 +24,5 @@ router.use("/mongoDB", mongoDB)
 app.use("/api", router);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:test test`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })

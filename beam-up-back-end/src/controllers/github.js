fd3f08c1fb7e635f3b47bb67601b394c77fd7508
Ram Montilia Smithson <ramsmithson1@gmail.com>
2022-01-05 14:52:07 +0200

    checking orgs fetching url from git API

--- beam-up-back-end/src/controllers/github.js
+++ beam-up-back-end/src/controllers/github.js
@@ -14,7 +14,7 @@ const getOrgs = async (req, res) => {
     const {accessToken} = req.query
     const headers = { Authorization: `token ${accessToken}` }
     const organizationID = Math.random() * 90000000
-    // const response = await axios.get(`https://api.github.com/organizations?per_page=10`, { headers: headers })
+    const response = await axios.get(`https://api.github.com/organizations?per_page=10`, { headers: headers })
         .catch((error) => { return ({ error: `${error}` }) })
     if (response.data) res.send(response.data)
     else res.status(500).send(`error: ${response}`)
